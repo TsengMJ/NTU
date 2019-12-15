635f78f262b78be70699f7fd42d7c5cbe98258ca
TsengMJ <tsngmj@gmail.com>
2019-12-15 21:02:23 +0800

    [MJ] Finished VC_hw1 2019.12.15

---
+++ ARCS/src/c++/test.cpp
@@ -0,0 +1,16 @@
+#include <opencv4/opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv)
+{
+//  // Read the image file
+    cv::Mat image = cv::imread("/home/mj/HardDisk/Github/NTU/ARCS/img/X-Ray/Test/avg_19.bmp");
+
+    cout << "OpenCV version : " << CV_VERSION << endl;
+
+
+ return 0;
+}
